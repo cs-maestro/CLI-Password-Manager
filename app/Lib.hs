58e8e95826d32faa32ca5c43c67f90f95abc17bc
cs-maestro <79442514+cs-maestro@users.noreply.github.com>
2023-12-09 14:53:53 -0700

    Update Lib.hs

--- app/Lib.hs
+++ app/Lib.hs
@@ -15,11 +15,6 @@ import System.Random
 import Control.Monad (void)
 
 
-
-
--- Define data types (as in the previous example)
-
--- Define functions
 boyerMooreSearch :: Eq a => [a] -> [a] -> [Int]
 boyerMooreSearch needle haystack = search 0
   where
@@ -56,36 +51,7 @@ generateRandomPassword useSymbols useNumbers length' = do
       password = map (chars !!) $ take length' $ randomRs (0, length chars - 1) gen
   return password
 
--- Implement the main functionalities
 
-searchByKeyword :: IO ()
-searchByKeyword = do
-  putStrLn "Enter search keyword:"
-  keyword <- getLine
-
-  let haystack = "This is a sample text with a keyword."
-  let positions = boyerMooreSearch keyword haystack
-  putStrLn $ "Keyword found at positions: " ++ show positions
-
-  putStrLn "Press Enter to go back to the main menu."
-  void getLine
-
-websiteLookup :: IO ()
-websiteLookup = do
-  putStrLn "Enter search keyword:"
-  keyword <- getLine
-
-  searchResults <- googleSearch keyword
-  putStrLn "Top 5 Google results:"
-  mapM_ putStrLn $ zipWith (\i result -> show i ++ ". " ++ result) [1..] searchResults
-
-  putStrLn "Choose a URL from the list (enter the corresponding number):"
-  chosenIndex <- readLn
-  let chosenURL = searchResults !! (chosenIndex - 1)
-  putStrLn $ "Chosen URL: " ++ chosenURL
-
-  putStrLn "Press Enter to go back to the main menu."
-  void getLine
 
 
   
