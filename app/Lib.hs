ec3ae2c0308a3049f9ef1473c106ec69d2df93a2
cs-maestro <79442514+cs-maestro@users.noreply.github.com>
2023-12-10 00:49:51 -0700

    Update Lib.hs

--- app/Lib.hs
+++ app/Lib.hs
@@ -3,17 +3,27 @@
 module Lib
     ( boyerMooreSearch
     , generateRandomPassword
-    , googleSearch
-    , searchByKeyword
-    , websiteLookup
+    , validateURL
     ) where
 
-import Data.List
-import Data.Maybe (fromMaybe)
-import System.IO
+import Data.List (isInfixOf, inits)
 import System.Random
-import Control.Monad (void)
+import Network.URI (URI, parseURI, uriScheme, uriAuthority, uriRegName, uriPath)
 
+validateURL :: String -> Bool
+validateURL url =
+  case parseURI url of
+    Just uri -> hasValidScheme uri && hasValidHostName uri
+    Nothing  -> False
+
+hasValidScheme :: URI -> Bool
+hasValidScheme uri = uriScheme uri == "http:" || uriScheme uri == "https:" 
+
+hasValidHostName :: URI -> Bool
+hasValidHostName uri =
+  case uriAuthority uri of
+    Just auth -> not (null (uriRegName auth))
+    Nothing   -> False
 
 boyerMooreSearch :: Eq a => [a] -> [a] -> [Int]
 boyerMooreSearch needle haystack = search 0
@@ -27,31 +37,11 @@ boyerMooreSearch needle haystack = search 0
         chunk = take len (drop i haystack)
         offset xs ys = length (takeWhile (/= ys) (inits xs))
 
-data UsernameCategory = Email | Phone | Username | Misc deriving (Show, Eq)
-data Account = Account
-  { website :: String
-  , username :: String
-  , usernameCategory :: UsernameCategory
-  , password :: String
-  , customName :: String
-  } deriving (Show, Eq)
-
-googleSearch :: String -> IO [String]
-googleSearch query = do
-  -- Placeholder for the actual implementation
-  putStrLn $ "Searching Google for: " ++ query
-  return ["www.example.com", "www.sample.com", "www.test.com", "www.demo.com", "www.xyz.com"]
-
-generateRandomPassword :: Bool -> Bool -> Int -> IO String
-generateRandomPassword useSymbols useNumbers length' = do
+generateRandomPassword :: Int -> IO String
+generateRandomPassword length' = do
   gen <- newStdGen
   let symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?/"
       numbers = "0123456789"
-      chars = ['a'..'z'] ++ ['A'..'Z'] ++ (if useSymbols then symbols else "") ++ (if useNumbers then numbers else "")
+      chars = ['a'..'z'] ++ ['A'..'Z'] ++ symbols ++ numbers
       password = map (chars !!) $ take length' $ randomRs (0, length chars - 1) gen
   return password
-
-
-
-
-  
